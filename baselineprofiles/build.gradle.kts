b7b2b688260e189a0964fe38f99e782cfbd3b014
Diraj H S <dirajhsdj@gmail.com>
2023-09-07 11:51:50 +0530

    Include missing files
    Signed-off-by: Diraj H S <dirajhsdj@gmail.com>

---
+++ baselineprofiles/build.gradle.kts
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("nowinandroid.android.test")
+    id("androidx.baselineprofile")
+}
+
+android {
+    namespace = "com.google.samples.apps.nowinandroid.baselineprofiles"
+
+    defaultConfig {
+        minSdk = 28
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "APP_BUILD_TYPE_SUFFIX", "\"\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+    com.google.samples.apps.nowinandroid.configureFlavors(this) { flavor ->
+        buildConfigField(
+            "String",
+            "APP_FLAVOR_SUFFIX",
+            "\"${flavor.applicationIdSuffix ?: ""}\""
+        )
+    }
+
+    targetProjectPath = ":app"
+    experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+baselineProfile {
+
+    // Enables using connected devices to generate profiles. The default is
+    // `true`. When using connected devices, they must be rooted or API 33 and
+    // higher.
+    useConnectedDevices = true
+}
+
+
+dependencies {
+    implementation(libs.androidx.benchmark.macro)
+    implementation(libs.androidx.test.core)
+    implementation(libs.androidx.test.espresso.core)
+    implementation(libs.androidx.test.ext)
+    implementation(libs.androidx.test.rules)
+    implementation(libs.androidx.test.runner)
+    implementation(libs.androidx.test.uiautomator)
+}
